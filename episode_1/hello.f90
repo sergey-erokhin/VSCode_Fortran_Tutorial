5a5e1aef2cc1fd8a81e3dece323b2f50f3460273
Sergey Erokhin <erokhin.sergey@gmail.com>
2022-04-21 11:46:22 +0200

    add second routine to episode 1

--- episode_1/hello.f90
+++ episode_1/hello.f90
@@ -4,3 +4,10 @@ REAL(8),intent(in) :: arg1
 REAL(8),intent(out) ::  arg2
 
 end subroutine first_routine
+
+subroutine second_routine(arg1,  arg2)
+    implicit none
+    REAL(8),intent(in) :: arg1
+    REAL(8),intent(out) ::  arg2
+    
+    end subroutine second_routine
